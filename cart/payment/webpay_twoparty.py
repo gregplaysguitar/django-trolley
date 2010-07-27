f4b685df60635939723ec147f6b54650e4534924
Greg Brown <gregplaysguitar@gmail.com>
2010-07-27 17:12:37 +1200

    typo

--- cart/payment/webpay_twoparty.py
+++ cart/payment/webpay_twoparty.py
@@ -42,8 +42,8 @@ class PaymentBackend:
         else:
             raise PaymentException('Webpay certificate password is not set')
         
-        if cart_settings.CART_WEBPAY_PORT:
-            webpay.setPort(webpayRef, cart_settings.CART_WEBPAY_PORT)
+        if cart_settings.WEBPAY_PORT:
+            webpay.setPort(webpayRef, cart_settings.WEBPAY_PORT)
         else:
             raise PaymentException('Webpay port is not set')
         

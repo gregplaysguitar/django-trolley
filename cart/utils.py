928041ea2c562e985989434d32428287056b92a2
Greg Brown <gregplaysguitar@gmail.com>
2011-02-20 14:48:36 -0800

    Added easy_tag decorator to utils

--- cart/utils.py
+++ cart/utils.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 import settings as cart_settings
 from django.db import models
+from django import template
+
 
 ## UTIL FUNCTIONS FOR CART STUFF
 
@@ -42,3 +44,36 @@ def get_order_detail_class():
             return models.get_model(app_label, model_name)
         except (ImportError):
             raise OrderDetailNotAvailable
+
+
+
+
+
+def easy_tag(func):
+    """deal with the repetitive parts of parsing template tags"""
+    def inner(parser, token):
+        # divide token into args and kwargs
+        args = []
+        kwargs = {}
+        for arg in token.split_contents():
+            try:
+                name, value = arg.split('=')
+                kwargs[str(name)] = value
+            except ValueError:
+                args.append(arg)
+        try:
+            # try passing parser as a kwarg for tags that support it
+            extrakwargs = kwargs.copy()
+            extrakwargs['parser'] = parser
+            return func(*args, **extrakwargs)
+        except TypeError:
+            # otherwise just send through the original args and kwargs
+            try:
+                return func(*args, **kwargs)
+            except TypeError, e:
+                raise template.TemplateSyntaxError('Bad arguments for tag "%s"' % args[0])
+    inner.__name__ = func.__name__
+    inner.__doc__ = inner.__doc__
+    return inner
+
+

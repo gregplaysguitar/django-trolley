9cdaa4e84233a44a19b0b66d16e136e2ce80fa2c
Greg Brown <gregplaysguitar@gmail.com>
2011-11-23 10:58:45 +1300

    redirect checkout/ for backwards compatibility

--- cart/urls.py
+++ cart/urls.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls.defaults import *
+from django.views.generic.base import RedirectView
+from django.utils.functional import lazy
+from django.core.urlresolvers import reverse
+
+reverse_lazy = lazy(reverse, str)
 
 
 urlpatterns = patterns('cart.views',
     (r'^$', 'checkout'),
-    (r'^$', 'index'), # dummy url for backwards-compatibility - allows reversing of cart.views.index
+    
+    # dummy url for backwards-compatibility - allows reversing of cart.views.index
+    (r'^$', 'index'), 
+    
+    # catch old static links to checkout/
+    (r'^checkout/$', RedirectView.as_view(url=reverse_lazy('cart.views.checkout'))),
+    
     (r'^delivery/$', 'delivery'),
     (r'^payment/$', 'payment'),
     (r'^payment/([\w_\-]+)/$', 'payment'),

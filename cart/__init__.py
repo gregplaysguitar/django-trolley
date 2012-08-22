72c7925805b418ebb32c60bfac7beec14f773825
Greg Brown <gregplaysguitar@gmail.com>
2012-08-22 16:49:37 +1200

    fixed impossible import in setup.py

--- cart/__init__.py
+++ cart/__init__.py
@@ -1,18 +1,12 @@
 from django.utils.importlib import import_module
 from django.core.exceptions import ImproperlyConfigured
 
-import settings as cart_settings
-
-
 __version__ = '1.1'
 VERSION = tuple(map(int, __version__.split('.'))) + ('dev',)
 
-
-#import_module('cart_helpers')
-
 def get_helper_module():
     '''Get the helper module as defined in the settings.'''
-    
+    import settings as cart_settings
     if cart_settings.HELPER_MODULE:
         try:
             package = import_module(cart_settings.HELPER_MODULE)
